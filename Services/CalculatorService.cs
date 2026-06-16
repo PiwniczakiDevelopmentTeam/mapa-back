df14375f0514b26c8c72f24a2e24f96367129be2
konrad.grzegorski <konrad.grzegorski+tbs@tbsmediacloud.com>
2026-06-16 21:13:31 +0200

    fix: test

---
+++ Services/CalculatorService.cs
@@ -0,0 +1,34 @@
+﻿namespace mapa_back.Services
+{
+	public class CalculatorService
+	{
+		public double Add(double a, double b)
+		{
+			return a + b;
+		}
+
+		public double Subtract(double a, double b)
+		{
+			return a - b;
+		}
+
+		public double Multiply(double a, double b)
+		{
+			return a * b;
+		}
+
+		public double Divide(double a, double b)
+		{
+			if (b == 0)
+			{
+				throw new Exception("Nie można dzielić przez zero!");
+			}
+
+			return a / b;
+		}
+		public double Percent(double value, double percent)
+		{
+			return (value * percent) / 100.0;
+		}
+	}
+}
